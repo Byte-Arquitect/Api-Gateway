98cf8b0ecbd5bbda6ba5062e1585e5694d0ab372
Frankoyusta <Franko.yusta@alumnos.ucn.cl>
2024-12-14 21:59:23 -0300

    Feat: Update Ocelot configuration to enhance authentication options for user routes

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b719a305c47cbacece1dbdff372edf753c143d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be921ee60f823d4c595213a844aa74f5b32688e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
